0ede442d3a6a95443c2f4754fbf2310dae293a24
Nameless-itf23 <ohutoncat@gmail.com>
2023-10-02 23:39:26 +0900

    responsive skill

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -11,9 +11,9 @@ function Skill({img, title, date, description}: Props) {
     return (
         <div className='Skill'>
             <img src={img} className='skill-img'></img>
-            <h2>{title}</h2>
-            <div>{date}</div>
-            <div>{description}</div>
+            <h2 className='skill-title'>{title}</h2>
+            <div className='skill-date'>{date}</div>
+            <div className='skill-description'>{description}</div>
         </div>
     )
 }
