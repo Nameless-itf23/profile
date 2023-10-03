4a97c5c088ab9d8dbc809abb24c325563c1adc39
Nameless-itf23 <ohutoncat@gmail.com>
2023-10-03 21:49:08 +0900

    add JS and React

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,6 +4,8 @@ import Skill from "./Skill";
 import Python from '../assets/Python.png'
 import Unity from '../assets/Unity.png'
 import HTML_CSS from '../assets/HTML_CSS.png'
+import JavaScript from '../assets/JavaScript.png'
+import _React from '../assets/React.png'
 
 function Skills() {
     const { t } = useTranslation();
@@ -19,7 +21,9 @@ function Skills() {
     const skills: type_skill[] = [
         {key: 0, img: Python, title: 'Python', date: '2020 / 05 ~', description: 'skills.skill.Python'},
         {key: 1, img: Unity, title: 'Unity C#', date: '2023 / 05 ~', description: 'skills.skill.Unity'},
-        {key: 2, img: HTML_CSS, title: 'HTML / CSS', date: '2023 / 07 ~', description: 'skills.skill.HTML_CSS'}
+        {key: 2, img: HTML_CSS, title: 'HTML / CSS', date: '2023 / 07 ~', description: 'skills.skill.HTML_CSS'},
+        {key: 3, img: JavaScript, title: 'JavaScript', date: '2023 / 06 ~', description: 'skills.skill.JavaScript'},
+        {key: 4, img: _React, title: 'React', date: '2023 / 07 ~', description: 'skills.skill.React'}
     ];
     
     return (
